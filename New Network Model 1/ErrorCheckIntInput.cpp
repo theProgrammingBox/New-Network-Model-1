f57033709c8f888dd2bf596775a88c4001c4e837
theProgrammingBox <justinboxgea@gmail.com>
2022-06-02 19:32:08 -0700

    Changed error checking format.

--- New Network Model 1/ErrorCheckIntInput.cpp
+++ New Network Model 1/ErrorCheckIntInput.cpp
@@ -4,15 +4,15 @@ bool ErrorCheckIntInput(int& input, int min, int max)
 {
 	if (!(cin >> input))
 	{
-		cout << "\n**** Please input a number between " << min << " and " << max << " ****\n\n";
+		cout << "\nPlease input a number between " << min << " and " << max << ".\n\n";
 		cin.clear();
 		cin.ignore(1000, '\n');
 		return true;
 	}
 	else if (input < min || input > max)
 	{
-		cout << "\n**** The number " << setw(5) << input << " is an invalid entry  ****\n";
-		cout << "**** Please input a number between " << min << " and " << max << " ****\n\n";
+		cout << "\nThe number " << input << " is an invalid entry.\n";
+		cout << "Please input a number between " << min << " and " << max << ".\n\n";
 		cin.ignore(1000, '\n');
 		return true;
 	}
